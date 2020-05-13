c71ba5f15f87e773be084ff9c324a5af421fa556
Edson Ramos <edramosc@gmail.com>
2020-05-13 18:34:11 -0500

    second_changes

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,8 +4,8 @@ from django.utils.translation import ugettext as _
 from django.utils.timezone import now
 
 class User(AbstractUser):
-    code = models.IntegerField(verbose_name=_('Codigo Empleado'),default=1234)
-    fullname = models.CharField(max_length=100, verbose_name=_('Nombre Empleado'))
+    code = models.IntegerField(verbose_name=_('Codigo Empleado'),default=1234, primary_key=True)
+    fullname = models.CharField(max_length=100, verbose_name=_('Nombre Empleado'),null=True)
     area = models.CharField(max_length=25,verbose_name=_('Area'),blank=True,null=True)
     birth_date = models.DateField(verbose_name=_('Fecha Nacimiento'),default=now)
     is_base = models.BooleanField(verbose_name=_('Es Base'),default=False)
