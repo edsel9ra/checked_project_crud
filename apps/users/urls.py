cbc884acb585987e6d86067af03a26eddb611433
Edson Ramos <edramosc@gmail.com>
2020-07-10 11:46:34 -0500

    Logout add
    Add logout to system and change login style

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.users.views import LoginView, UserCreate
+from apps.users.views import LoginView, UserCreate, LogoutView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login', view=LoginView.as_view(), name=LoginView.name),
     path('create', view=UserCreate.as_view(), name=UserCreate.name),
+    path('logout', view=LogoutView.as_view(), name='logout')
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,28 +1,29 @@
-from django.views.generic import TemplateView, CreateView, UpdateView, ListView
+from django.views.generic import TemplateView, CreateView, UpdateView, ListView, View
 from .models import User
 from apps.users.forms import LoginForm, UserForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 
+
 class LoginView(TemplateView):
     name = 'login'
     template_name = 'users/login.html'
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
         form = LoginForm()
         return render(
             request,
             self.template_name,
             {
-                'form':form
+                'form': form
             }
         )
-    
-    def post(self,request,*args,**kwargs):
+
+    def post(self, request, *args, **kwargs):
         form = LoginForm(request.POST)
 
         if not form.is_valid():
@@ -37,7 +38,8 @@ class LoginView(TemplateView):
 
         data = form.cleaned_data
 
-        user = authenticate(request,username=data.get('username'),password=data.get('password'))
+        user = authenticate(request, username=data.get(
+            'username'), password=data.get('password'))
 
         if not user:
             return render(
@@ -49,16 +51,17 @@ class LoginView(TemplateView):
                 }
             )
 
-        login(request,user)
+        login(request, user)
 
         back = request.GET.get('next')
 
         if back:
             return HttpResponseRedirect(back)
-            
+
         return redirect('checklist:check_out_list')
 
-@method_decorator(login_required,name='dispatch')
+
+@method_decorator(login_required, name='dispatch')
 class UserCreate(CreateView):
     name = 'create'
     model = User
@@ -66,6 +69,11 @@ class UserCreate(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('checklist:check_out_list')
-        
 
 
+@method_decorator(login_required, name='dispatch')
+class LogoutView(View):
+    
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('users:login')
